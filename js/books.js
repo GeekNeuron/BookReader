c310f000197753334459ef83091122fdf69935ec
Geek Neuron <geekneuron@gmail.com>
2025-07-06 21:44:04 +0330

    Create books.js

---
+++ js/books.js
@@ -0,0 +1,45 @@
+const booksData = [
+    {
+        id: "zoroaster",
+        title: "چنین گفت زرتشت",
+        author: "فریدریش نیچه",
+        year: 1883,
+        cover: "assets/images/zoroaster.jpg", // آدرس جلد کتاب
+        file: "assets/epubs/zoroaster.epub", // آدرس فایل ای‌پاب
+        description: "چنین گفت زرتشت، کتابی است فلسفی و شاعرانه که فریدریش نیچه، فیلسوف آلمانی آن را میان سال‌های ۱۸۸۳ تا ۱۸۸۵ نگاشته‌است.",
+        tags: ["فلسفی", "رمان"],
+        progress: {
+            readPages: 14,
+            totalPages: 1523
+        }
+    },
+    {
+        id: "the-stranger",
+        title: "بیگانه",
+        author: "آلبر کامو",
+        year: 1942,
+        cover: "assets/images/stranger.jpg",
+        file: "assets/epubs/stranger.epub",
+        description: "بیگانه نام رمانی است از آلبر کامو که در سال ۱۹۴۲ در انتشارات گالیمار به چاپ رسید. این رمان در لیست ۱۰۰ کتاب قرن لوموند جایگاه اول را به خود اختصاص داده‌است.",
+        tags: ["فلسفی", "اگزیستانسیالیسم"],
+        progress: {
+            readPages: 50,
+            totalPages: 200
+        }
+    },
+    {
+        id: "fantasy-book",
+        title: "ارباب حلقه‌ها",
+        author: "جی. آر. آر. تالکین",
+        year: 1954,
+        cover: "assets/images/lotr.jpg",
+        file: "assets/epubs/lotr.epub",
+        description: "ارباب حلقه‌ها یک مجموعهٔ رمان خیال‌پردازی حماسی به نویسندگی جی. آر. آر. تالکین است.",
+        tags: ["فانتزی", "حماسی"],
+        progress: {
+            readPages: 0,
+            totalPages: 1178
+        }
+    }
+    // ... سایر کتاب ها
+];
